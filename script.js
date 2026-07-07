5f67b0630caee402550adb8731fe29fd292bdf12
Phennylalanine <petergregoryj@gmail.com>
2026-07-07 12:13:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
     "placesMlevelr",
     "oppositeMlevelr",
     "schoolEventMlevelr",
-  ].map((key) => ({ key, multiplier: key.includes("M") ? 0.85 : 0.5 }));
+  ].map((key) => ({ key, multiplier: key.includes("M") ? 0.85 : 0.9 }));
 
   // === LOCAL STORAGE HELPERS ===
   const LS = {
